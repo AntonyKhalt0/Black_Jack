75867a0744939b6ecdab947a5a869422e0955b44
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-03-21 17:56:34 +0300

    Add main file

--- interface.rb
+++ interface.rb
@@ -1,10 +1,12 @@
 class Interface
-  def start_game
-    puts "Привет дорогой гость. Пожалуйста, введите имя чтобы играть!"
-    user_name = gets.chomp
-    @user = User.new(user_name)
-    @user.initial_add_cards(@user)
+  def initialize
+    @bank = 0
+  end
+
+  def start_game(user_name = nil)
+    @user ||= User.new(user_name)
     @dealer = Dealer.new
+    @user.initial_add_cards(@user)
     @dealer.initial_add_cards(@dealer)
     @deck = Deck.new
     user_action
@@ -12,13 +14,15 @@ class Interface
 
   def user_action
     # Действие пользователя
-    user_action_list
-    choice = gets.chomp.to_i
-    case choice
-    when 1 then dealer_action
-    when 2 then add_card_on_hands(user)
-    when 3 then open_cards 
-    when 4 then exit
+    loop do
+      user_action_list
+      choice = gets.chomp.to_i
+      case choice
+      when 1 then dealer_action
+      when 2 then add_card_on_hands(user)
+      when 3 then open_cards 
+      when 4 then game_exit
+      end
     end
   end
 
@@ -39,8 +43,14 @@ class Interface
   end
 
   def open_cards
-    @user.open_cards
-    @dealer.open_cards
+    puts "Карты игрока: #{@user.show_playing_cards}\n
+          Сумма очков игрока: #{@user.total_points}\n
+          Карты дилера: #{@dealer.show_playing_cards}\n
+          Сумма очков игрока: #{@dealer.total_points}"
+    if @user.total_points <= 21 && @user.total_points > @dealer.total_points
+      puts "Вы выиграли! Хотите сыграть снова?"
+    else
+    end
   end
 
   def initial_add_cards(gamer)
@@ -58,5 +68,13 @@ class Interface
           Введите 2, чтобы добавить карту.\n
           Введите 3, чтобы открыть карты.\n 
           Введите 4, чтобы выйти."
+  end
+
+  def game_result
+  end
+
+  def game_exit
+    puts "До свидания! Приходите снова!"
+    exit
   end
 end
